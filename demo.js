382ee8e3b4158f2adee29440088335cd21dc3216
baiye <gezebin1998317@outlook.com>
2021-07-25 19:34:09 +0800

    初始版本

---
+++ demo.js
@@ -0,0 +1,26 @@
+//---------闭包----------
+// var scope = "one";
+
+// function changeScope(){
+//     var scope = "two";
+//     function f(){
+//         console.log(scope);
+//     }
+//     return f;
+// }
+
+// console.log(scope);
+// let change = changeScope();
+// change();
+//----------日期-----------------
+// const now = new Date();
+// const frontend = new Date(2021,6,15);
+
+// console.log(now.getDay());
+// console.log(frontend);
+//-------------Array---------
+
+let arr = Array(2);
+arr[2] = 3;
+console.log(arr.length);
+console.log(arr[2]);
---
+++ this.js
@@ -0,0 +1,7 @@
+var obj = {
+    name:"baiye",
+    age:23,
+    sayName:function(){
+        console.log(this.name)
+    }
+}
